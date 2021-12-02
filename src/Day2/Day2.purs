dc17395f7a17e73102917e0889f9b0077faeca3c
Nimor111 <georgi.bojinov@hotmail.com>
2021-12-03 00:12:36 +0200

    Add day 2

---
+++ src/Day2/Day2.purs
@@ -0,0 +1,93 @@
+module Day2 where
+
+import Prelude
+
+import Common (readInput)
+import Data.Int (fromString)
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+
+import Data.String (split)
+import Data.String.Pattern (Pattern(..))
+
+day2Part1 :: Effect Int
+day2Part1 = do
+  inputText <- readInput "./src/Day2/input.txt"
+  parsedInstructions <- pure $ parseInstructions inputText
+  horizontalPosition <- pure $ calculateHorizontalPosition parsedInstructions
+  depth <- pure $ calculateDepth parsedInstructions
+  pure $ horizontalPosition * depth
+
+day2Part2 :: Effect Int
+day2Part2 = do
+  inputText <- readInput "./src/Day2/input.txt"
+  parsedInstructions <- pure $ parseInstructions inputText
+  result <- pure $ horizontalPositionAndDepthWithAim 0 0 0 parsedInstructions
+  pure result
+
+data Direction = Down | Up | Forward | Unknown
+
+derive instance eqDirection :: Eq Direction
+
+directionToString :: Direction -> String
+directionToString Down = "down"
+directionToString Forward = "forward"
+directionToString Up = "up"
+directionToString Unknown = "unknown"
+
+instance showDirection :: Show Direction where
+  show = directionToString
+
+readDirection :: String -> Direction
+readDirection "down" = Down
+readDirection "up" = Up
+readDirection "forward" = Forward
+readDirection _ = Unknown
+
+type Instruction = {
+  direction :: Direction
+, value :: Int
+}
+
+parseInstruction :: String -> Int -> Instruction
+parseInstruction direction value = {
+  direction: readDirection direction,
+  value: value
+}
+
+parseInstructions :: List String -> List Instruction
+parseInstructions (stringInstruction : xs) =
+  case split (Pattern " ") stringInstruction of
+    [dir, val] -> case fromString val of
+      Just v -> (parseInstruction dir v) : parseInstructions xs
+      Nothing -> parseInstructions xs
+    _ -> parseInstructions xs
+parseInstructions _ = Nil
+
+calculateHorizontalPosition :: List Instruction -> Int
+calculateHorizontalPosition ({ direction, value } : xs) =
+  if direction == Forward then
+    value + calculateHorizontalPosition xs
+  else
+    calculateHorizontalPosition xs
+calculateHorizontalPosition Nil = 0
+
+calculateDepth :: List Instruction -> Int
+calculateDepth ({ direction, value } : xs) = case direction of
+  Up -> -value + calculateDepth xs
+  Down -> value + calculateDepth xs
+  _ -> calculateDepth xs
+calculateDepth Nil = 0
+
+type Horizontal = Int
+type Depth = Int
+type Aim = Int
+
+horizontalPositionAndDepthWithAim :: Horizontal -> Depth -> Aim -> List Instruction -> Int
+horizontalPositionAndDepthWithAim horizontal depth aim ({ direction, value } : xs) = case direction of
+  Up -> horizontalPositionAndDepthWithAim horizontal depth (aim - value) xs
+  Down -> horizontalPositionAndDepthWithAim horizontal depth (aim + value) xs
+  Forward -> horizontalPositionAndDepthWithAim (horizontal + value) (depth + aim * value) aim xs
+  _ -> horizontalPositionAndDepthWithAim horizontal depth aim xs
+horizontalPositionAndDepthWithAim horizontal depth _ Nil = horizontal * depth
